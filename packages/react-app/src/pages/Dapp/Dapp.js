a6eb1517b3fb249b22dd49b0bcfe9c7e88b253aa
TheCleanerSweeper <83165291+TheCleanerSweeper@users.noreply.github.com>
2021-05-12 18:18:51 +0200

    mobile layout fix (#1)

--- packages/react-app/src/pages/Dapp/Dapp.js
+++ packages/react-app/src/pages/Dapp/Dapp.js
@@ -233,18 +233,21 @@ export default function Dapp() {
 								<a href="#" className="flex-shrink-0 group block">
 									<div className="flex items-center">
 										<div>
-											<img
-												className="inline-block h-10 w-10 rounded-full"
-												src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-												alt=""
+											<EthIcon
+												className="inline-block h-9 w-9 rounded-full"
+												// Address to draw
+												address={address}
+												// scale * 8 pixel image size
+												scale={16}
+												// <img> props
+												style={{
+													background: "red",
+												}}
 											/>
 										</div>
 										<div className="ml-3">
-											<p className="text-base font-medium text-white">
-												0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815
-											</p>
-											<p className="text-sm font-medium text-gray-400 group-hover:text-gray-300">
-												View profile
+											<p className="text-sm font-medium text-white">
+												{address ? shortenAddress(address) : "None Set"}
 											</p>
 										</div>
 									</div>
