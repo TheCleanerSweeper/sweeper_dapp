1524c2770c3b56541ba830c96a1498e6c48d1c25
Weiss <weiss@sweeper.network>
2021-05-19 23:07:39 +0200

    add geyer abi

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,9 +1,11 @@
 import airdropAbi from "./abis/airdrop.json";
 import sweeperdaoAbi from "./abis/sweeperdao.json";
+import geyserAbi from "./abis/geyser.json";
 
 const abis = {
   airdrop: airdropAbi,
   sweeperdao: sweeperdaoAbi,
+  geyser: geyserAbi,
 };
 
 export default abis;
